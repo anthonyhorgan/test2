3ecb5f859e42e7d444a611ed67cea6e0a2dd0924
Anthony Horgan <anthony.horgan@xperi.com>
2020-03-05 08:46:40 +0000

    second commit

---
+++ car.py
@@ -0,0 +1,9 @@
+
+class Car():
+
+    def __init__(self, reg):
+        self.reg = reg
+
+    def __repr__(self):
+        return f"Car reg={self.reg}"
+
---
+++ main.py
@@ -0,0 +1,5 @@
+import car
+
+my_car = car.Car("181 GY 112435")
+
+print(my_car)
